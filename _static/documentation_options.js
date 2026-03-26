89cfa8ac1a651967c9ac8208e9a25afb3528464b
farid <farid.zare.edu@gmail.com>
2026-03-26 14:21:21 +0000

    Fix the page width

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -11,3 +11,12 @@ const DOCUMENTATION_OPTIONS = {
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
+
+(function () {
+    var root = document.documentElement.dataset.content_root || './';
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = root + '_static/custom-wide.css';
+    document.head.appendChild(link);
+})();
