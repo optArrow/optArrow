d218c5df88b2b5d72d811bd1c12ac7d5e2fbee1b
farid <farid.zare.edu@gmail.com>
2026-05-28 11:05:56 +0100

    Update the website by adding a link to the github repo

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -18,3 +18,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom-wide.css']
+html_context = {
+    "display_github": True,
+    "github_user": "optArrow",
+    "github_repo": "optArrow",
+    "github_version": "main",
+    "conf_py_path": "/Documentation/",
+}
