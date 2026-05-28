cac18b686e923865fb6251822aad3bc2f17ee6e8
farid <farid.zare.edu@gmail.com>
2026-05-28 11:23:25 +0100

    Add sitemap

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -10,7 +10,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
+    'sphinx_sitemap',
 ]
+
+html_baseurl = 'https://optarrow.github.io/optArrow/'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -18,6 +21,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom-wide.css']
+html_extra_path = ['_static/robots.txt']
 html_context = {
     "display_github": True,
     "github_user": "optArrow",
